2f1efe11b5223dceda9fa43911910e1e1cf982b6
LuisGitHubbr <108435619+LuisGitHubbr@users.noreply.github.com>
2022-06-30 00:37:10 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { API } from 'homebridge';
+import { API } from 'homebridge'
 
-import { ACCESSORY_NAME } from './settings';
-import { SwitchAccessory } from './switchAccessory';
+import { ACCESSORY_NAME } from './settings'
+import { SwitchAccessory } from './switchAccessory'
 
 export = (api: API) => {
-  api.registerPlatform(ACCESSORY_NAME, SwitchAccessory);
-};
+  api.registerPlatform(ACCESSORY_NAME, SwitchAccessory)
+}
