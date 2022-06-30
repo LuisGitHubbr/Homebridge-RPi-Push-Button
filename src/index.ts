bfbdf1ae37c19154c3aacb730bedd3c47736e5a4
LuisGitHubbr <108435619+LuisGitHubbr@users.noreply.github.com>
2022-06-30 10:15:41 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import { ACCESSORY_NAME } from './settings'
 import { SwitchAccessory } from './switchAccessory'
 
 export = (api: API) => {
-  api.registerPlatform(ACCESSORY_NAME, SwitchAccessory)
+	api.registerPlatform(ACCESSORY_NAME, SwitchAccessory)
 }
